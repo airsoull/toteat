f77cbc13028b51883a23fa132e0eedbb03439974
Cristian <cristian@magnet.cl>
2020-11-02 19:45:20 -0300

    chore: add base model

---
+++ base/models.py
@@ -0,0 +1,18 @@
+# django
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('created at'),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, null=True,
+        verbose_name=_('updated at'),
+    )
+
+    class Meta:
+        abstract = True
